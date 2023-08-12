8f857e31687f300f8fa8126fac773e26da3b2b34
KorespondentAda <0kononov0@gmail.com>
2023-08-12 17:48:49 +0500

    fixup! Fix JPEG error in non-debug build

--- utils.h
+++ utils.h
@@ -3,9 +3,11 @@
 #if defined DEBUG
 # define Debug(MESSAGE, ...) fprintf(stderr, "[Debug]: " MESSAGE "\n", ##__VA_ARGS__);
 # define DebugPos(X, Y) fprintf(stderr, "[Debug]: Cursor position: %.2f:%.2f\n", X, Y);
+# define DebugStatus(X) fprintf(stderr, "[Debug]: Command '" #X "' status: %d", (X));
 #else
 # define Debug(...)
 # define DebugPos(...)
+# define DebugStatus(X) X
 #endif
 
 void Error(char *message);
